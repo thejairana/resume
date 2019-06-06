7504c5880e84b9259e5351c7c2f60dfdcc8dae2d
jay.kumar <jay.kumar@elogist.in>
2019-06-06 10:00:37 +0530

    Updates

---
+++ precache-manifest.765cec6c0306eec80671a64bbf46e38f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "925ad9cb7714fecd7721df3b54f64031",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "6b2b141f86261f746a19",
+    "url": "/resume/static/css/2.ade15fdf.chunk.css"
+  },
+  {
+    "revision": "5f0a1495b972da131aef",
+    "url": "/resume/static/css/main.b3b68c24.chunk.css"
+  },
+  {
+    "revision": "6b2b141f86261f746a19",
+    "url": "/resume/static/js/2.12277a70.chunk.js"
+  },
+  {
+    "revision": "5f0a1495b972da131aef",
+    "url": "/resume/static/js/main.b2c89640.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/resume/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/resume/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/resume/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/resume/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/resume/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
