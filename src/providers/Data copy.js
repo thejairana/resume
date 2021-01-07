839750fb33a4539e936efb8d926a87c18618f36e
JRX <jkrana008@gmail.com>
2021-01-07 12:12:00 +0530

    Changes

--- src/providers/Data copy.js
+++ src/providers/Data copy.js
@@ -1,14 +1,13 @@
 const personalDetails = {
     name: 'JAY KUMAR',
-    designation: 'Full Stack Developer',
-    aboutMe: `I'm Full Stack Developer with 4 years of work experience in web, app & ML development.
-            Mostly i worked on Javascript & Python frameworks such as Angular, NodeJS, Ionic, ReactJS, ML, Automation etc.
-            I have worked on various automation, Machine Learning, Web & App projects. My best work is Banking Automation, 
-            Fastage Automation, Challan Automation, PDF Version Controller & others automation & ML projects.
+    designation: 'Full Stack Web Developer',
+    aboutMe: `I'm an innovative Full Stack Web Developer with 3 years of work experience (2 years, 7 months in hand) in web development.
+            Mostly i worked on Javascript frameworks such as Angular, NodeJS, Ionic etc.
+            Recently I trained a team on Angular & Ionic within 3 weeks and getting 80% success.
             I have developed products for more than 3 startups with upto 90% work satisfaction.`,
     details: [
         { icon: 'fa fa-envelope', html: `<a href="mailto:therana.jai@gmail.com" alt="therana.jai@gmail.com" target="_blank" rel="noopener noreferrer">therana.jai@gmail.com</a>` },
-        // { icon: 'fa fa-github', html: `<a href="https://github.com/thejairana" alt="github" target="_blank" rel="noopener noreferrer">github.com/thejairana</a>` },
+        { icon: 'fa fa-github', html: `<a href="https://github.com/thejairana" alt="github" target="_blank" rel="noopener noreferrer">github.com/thejairana</a>` },
         // { icon: 'fa fa-github', html: `<a href="https://thejairana.github.io/portfolio" alt="github" target="_blank" rel="noopener noreferrer">thejairana.github.io/portfolio</a>` },
         { icon: 'fa fa-linkedin', html: `<a href="https://www.linkedin.com/in/jai-rana/" alt="linkedin" target="_blank" rel="noopener noreferrer">linkedin.com/in/jai-rana/</a>` },
         { icon: 'fa fa-mobile', html: `<a href="tel:+918385803337" alt="+918385803337" target="_blank" rel="noopener noreferrer">+918385803337</a>, <a href="tel:+916378922131" alt="+916378922131" target="_blank" rel="noopener noreferrer">+916378922131</a>` },
@@ -17,34 +16,29 @@ const personalDetails = {
     experience: [
         {
             designation: 'Full Stack Developer',
-            company: 'Axestrack Software Solutions',
-            duration: '10/2020 - Present',
-            location: 'Jaipur',
-            desc: `Provide the GPS based tracking solutions for consignment, personal, mobile and vehicle based tracking with Geofence, Over Speed, stoppage, Route-deviation, 
-            Break-down, fuel consumption and different-2 Sensor based analysis solution.`,
-        },
-        {
-            designation: 'Full Stack Developer',
             company: 'Elogist Private Ltd.',
-            duration: '02/2018 - 09/2020',
+            duration: '02/2018 - Present',
             location: 'Jaipur',
-            desc: `Provide AI based solution for Fastag Toll, Fuel Consumption, Trip Management, Location Tracking, Challan Payments, 
-            Driver Performance Monitoring & Other Logistic Services.`,
+            desc: `I am joined this startup as a full stack developer. I had developed main product of elogist known as walle8.
+            Currently we are developing a digitalized system for logistic service providers know as 'DOST'. My Role in the current product 
+            is to manage front end development.`,
         },
         {
             designation: 'Full Stack Developer',
             company: 'Appwallaz',
             duration: '05/2017 - 01/2018',
             location: 'Jaipur',
-            desc: `Its a service based company provide complete solution for web, app & software development.`,
+            desc: `In this company we was developed many mobile & web applications for the client with upto 90% of satisfaction. We worked with more than 3 startups and developed their products. And your best achievement was 
+            Javascript plugin development Just in 2 hours. In this company we had work with SeADoc, Volkswagen Jaipur, QK Feedback, The Peak Dispensary, The ShortCut and others.`,
         },
         {
             designation: 'Full Stack Developer',
             company: 'Youstart',
             duration: '01/2017 - 05/2017',
             location: 'Jaipur',
-            desc: `Its a service based company consulting & providing solution for web, app, software, AI, & ML development. 
-            Mostly it worked with startups`,
+            desc: `This was my first company. I joined this company as a full stack developer. And my first project was "Sparup". 
+            I worked hard on this project and handle complete front end development alone. We delivered this project before the deadline and get appreciation from the client. 
+            I also worked here on many applications such as MyLib, Car Rental, Quiz App, Chat Application etc.`,
         }
     ],
     educations: [
@@ -65,32 +59,25 @@ const personalDetails = {
             desc: ''
         }
     ],
-    skills: ['Javascript', 'Python', 'Angular', 'NodeJS', 'Ionic', 'MongoDB', 'ML', 'ReactJS', 'TensorFlow'],
+    skills: ['Javascript', 'Angular', 'NodeJS', 'Ionic', 'Git', 'MongoDB'],
     projects: [
         {
-            name: 'Automation with ML',
-            tech: 'Python',
-            desc: `Implemented Banking, Fastag, Challan & Captcha automation using Python with machine learning`,
-            link: ''
-        },
-        {
-            name: 'PDF Version Controller',
-            tech: 'Javascript, Pdfjs, FabricJS',
-            desc: `A version controller for PDF using Javascript. Its like git for PDF.`,
-            link: ''
-        },
-        {
             name: 'DOST (Digital Operations for Smart Transportation)',
-            tech: 'Angular, Ionic, Javascript',
+            tech: 'Angular, Ionic, Cake Php, PostgreSQL',
             desc: `DOST is a complete ERP for logistic owners to manage their records. 
             It has more than 7 modules such as accounting, trips, tyres, documents etc.`,
             link: 'http://elogist.in/dost/'
         },
         {
             name: 'Walle8',
-            tech: 'Ionic, Javascript',
+            tech: 'Ionic, Cake Php, PostgreSQL',
             desc: `Wallet for fliet owners to fill fuel & pay toll tax and getting discount on every fuel filling or toll tax payment.`,
             link: 'https://play.google.com/store/apps/details?id=www.walle8.com'
+        },
+        {
+            name: 'Automation',
+            tech: 'Python, Selenium',
+            desc: `A automation system to login in bank accounts (more than 5 banks) using internet banking and download latest statement of bank transactions in every 5 minutes.`
         },
         {
             name: 'The Peak Dispensary',
