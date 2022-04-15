f11cf93ddd7ffb73370cce6a95e170378bcb0b91
Jai Rana <therana.jai@gmail.com>
2022-04-15 21:50:31 +0530

    Build

---
+++ build/precache-manifest.1095e958e894ab70250e672fd88895ff.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d5c6d0621e912a5ef4ac80f4a2ac06",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "04a3cff1d4281644de90",
+    "url": "/resume/static/css/2.8fb740c1.chunk.css"
+  },
+  {
+    "revision": "70dddc54ac3bee755f34",
+    "url": "/resume/static/css/main.222b0a09.chunk.css"
+  },
+  {
+    "revision": "04a3cff1d4281644de90",
+    "url": "/resume/static/js/2.c1cd600e.chunk.js"
+  },
+  {
+    "revision": "70dddc54ac3bee755f34",
+    "url": "/resume/static/js/main.9aacbec1.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/resume/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/resume/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/resume/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/resume/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/resume/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
